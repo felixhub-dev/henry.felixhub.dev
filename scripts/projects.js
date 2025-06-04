e1d145aa614366aee685d73e17ce9bd5594486d4
cootshk <cootshk@duck.com>
2025-06-04 03:12:37 -0500

    Dynamically fill projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,11 +1,57 @@
+let projects;
+let template;
 function projectsOnLoad() {
     console.log("projectsOnLoad");
-    let template;
     fetch("/api/projects.json").then(response => response.json()).then(data => {
         console.log(data);
+        projects = data;
+        if (!!projects && !!template) { // either this or the other one will finish first, so it should *in theory* only run once
+            renderProjects();
+        }
     });
-    fetch("/html/templates/project.html").then(response => response.text()).then(data => {template = data; console.log(data);});
+    fetch("/html/templates/project.html").then(response => response.text()).then(data => {
+        console.log(data);
+        template = data;
+        if (!!projects && !!template) {
+            renderProjects();
+        }
+    });
     console.log(template);
 
     $('projects-loading').classList.remove("hidden");
+}
+let replaceable = ["name", "description", "id", "links.icon", "links.url", "links.github", "links.discord", "links.matrix"];
+function renderProjects() {
+    console.log("renderProjects");
+    $('projects-loading').classList.add("hidden");
+
+    const projectsHTML = replaceProjects(projects.projects);
+    $('my-projects').innerHTML += projectsHTML;
+    const contributedHTML = replaceProjects(projects.contributions);
+    $('my-contributions').innerHTML += contributedHTML;
+
+
+    $('projects-container').classList.remove("hidden");
+}
+function replaceProjects(projects) {
+    var ret = "";
+    for (let i = 0; i < projects.length; i++) {
+        let project = projects[i];
+        console.log(projects[i]);
+        var newProject = structuredClone(template);
+        for (let j = 0; j < replaceable.length; j++) {
+            let key = replaceable[j];
+            let keys = key.split(".");
+            key = keys.pop(); // get the last key
+            var value = project[key];
+            if (keys[0] === "links") {
+                value = project.links[key] || "";
+            }
+            if (!value) newProject = newProject.replaceAll(`{{${key}_hidden}}`, "hidden")
+            else newProject = newProject.replaceAll(`{{${key}_hidden}}`, "");
+            newProject = newProject.replaceAll(`{{${key}}}`, value ?? "");
+        }
+        ret += newProject;
+    }
+    return ret;
 }
